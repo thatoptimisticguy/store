85833c311c243b459ed27d12acf02363600c90e6
thatoptimisticguy <59786329+thatoptimisticguy@users.noreply.github.com>
2020-09-30 20:13:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+            function tog_function(){
+                var color = document.querySelector('body > span > input').value;
+                console.log(color);
+                window._pcq = window._pcq || [];
+                window._pcq.push(['tag', 'check_color', color]);
+            }
